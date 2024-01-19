d65c2e00b489fab28f37cb8e9a5ec4b1e96a186f
Neil Sagar Sahu <neilsagar55555@gmail.com>
2024-01-20 01:09:02 +0530

    updated

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ScoreCard({heading,value}) {
   return (
-    <div style={value==="Passed"?{backgroundColor:"#13BB24",border:"2px solid #727272",color:"#fff"}:{backgroundColor:"#D9D9D9",border:"2px solid #727272"}} className='w-5/12 min-w-fit p-2' >
+    <div style={value==="Passed"?{backgroundColor:"#13BB24",border:"2px solid #727272",color:"#fff"}:{backgroundColor:"#D9D9D9",border:"2px solid #727272"}} className='w-1/3 lg:w-5/12 md:w-5/12 sm:w-5/12 min-w-fit p-2' >
         <h2 className='text-xl'>{heading}</h2>
         <p className='font-light'>{value}</p>
     </div>
