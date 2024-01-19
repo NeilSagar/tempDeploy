0b487d547a4e84edb4b8bca8a367d90451c41908
Neil Sagar Sahu <neilsagar55555@gmail.com>
2024-01-20 01:22:07 +0530

    updated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -14,7 +14,7 @@ const images =[bookmark,cloud,ok, plus];
       <p className='lg:w-1/2 md:w-2/3 w-11/12  mx-auto font-light  tracking-wide leading-8'>Designhill's PayPal calculator takes away the complexity of calculating individual fees and payments. It makes sure your calculations are done correctly even in a jiffy. If you own a business and your buyers /customers use PayPal, you'll have to bear the fee. As you have many transactions in a day, Designhill's PayPal fee calculator can prove to be a useful tool for you.. 
       Read more.</p>
 
-      <h2 className='lg:w-1/2 md:w-2/3 sm:w-full mx-auto text-4xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</h2>
+      <h2 className='lg:w-1/2 md:w-2/3 w-11/12 mx-auto text-4xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</h2>
       <div className=' flex flex-wrap justify-center'>
         {images.map((image,index)=>{
           return (
